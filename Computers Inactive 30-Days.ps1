1b2c7ae5dbd9594e392dc4179c768a85950df517
woitkows <woitkowski@gmail.com>
2015-09-22 07:52:05 -0400

    Update Computers Inactive 30-Days.ps1

--- Computers Inactive 30-Days.ps1
+++ Computers Inactive 30-Days.ps1
@@ -8,7 +8,7 @@ $time = (Get-Date).Adddays(-($DaysInactive))
 $dc = Get-ADComputer -Searchbase "OU=bacon,DC=your,DC=domain" -Filter {LastLogonTimeStamp -le $time} -Properties LastLogonTimeStamp | Format-List Name, @{N='lastlogonTimestamp'; E={[DateTime]::FromFileTime([Int64] $_.lastlogonTimestamp)}} | Out-String
  
 #Email Information
-$smtpServer = "internalSMTPRelay.media.global.loc"
+$smtpServer = "internalSMTPRelay..your.company"
 $from = "from@address.com"
 $emailaddress = "emailaddress@email.com"
 $subject = "Computers that have not signed into the domain in 30 Days or longer."
